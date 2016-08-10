6e28ad93a5f574bc877f87d87a512e9adf051048
Pawel Loj <pawel.loj@gmail.com>
2016-08-11 01:07:35 +0200

    Making progress

---
+++ webapp/cafe/routes/menu.js
@@ -0,0 +1,32 @@
+var config = require("../config");
+var extend = require("extend");
+var promise = require('bluebird');
+var options = {
+    promiseLib: promise
+};
+var pgp = require('pg-promise')(options);
+var util = require('util');
+var esc = require('pg-escape');
+
+var express = require('express');
+var router = express.Router();
+
+var db = pgp(config.db);
+
+/* GET menu page */
+router.get('/', promise.coroutine(function*(req, res, next) {
+    try {
+        var client = yield db.connect();
+        var sql = "SELECT d.id, d.dt, d.notes, t.name AS type_name FROM cf_day d JOIN cf_menu_type t ON d.menu_type_id=t.id ORDER BY d.dt DESC";
+        var rows = yield client.query(sql);
+        res.render('index', { days: rows, test: "Blah" });
+    } catch (err) {
+        console.log(err);
+        //TODO - better error handling
+        return res.status(500).send(err);
+    } finally {
+        if (client) client.done();
+    }
+}));
+
+module.exports = router;
