735e3e5bc31928bd37d0d0e562bc13a007d73caf
Pawel Loj <pawel.loj@gmail.com>
2016-08-11 01:07:02 +0200

    Making progress

--- webapp/cafe/app.js
+++ webapp/cafe/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var menu = require('./routes/menu');
 
 var app = express();
 
